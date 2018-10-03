8d8e537819925c751d3d518a0472c7452ee654e9
CnAoKip <43806705+CnAoKip@users.noreply.github.com>
2018-10-03 15:45:47 +0800

    Add files via upload

---
+++ Virtual Desktop/虚拟桌面_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef ÐéÄâ×ÀÃæ_PRIVATE_H
+#define ÐéÄâ×ÀÃæ_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.2.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	2
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"2191323374@qq.com"
+#define FILE_VERSION	"1.2.0.0"
+#define FILE_DESCRIPTION	"ÐéÄâ×ÀÃæ"
+#define INTERNAL_NAME	"ÐéÄâ×ÀÃæ"
+#define LEGAL_COPYRIGHT	"Copyright (C) Handick Software Co.,Ltd."
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"VirtualDesktop.exe"
+#define PRODUCT_NAME	"ÐéÄâ×ÀÃæ"
+#define PRODUCT_VERSION	"1.2.0.0"
+
+#endif /*ÐéÄâ×ÀÃæ_PRIVATE_H*/
